c7923d33ade38a0372f46ab279647b15ea0e7db3
Arvindhan Rameshbabu <arvindhan.rameshbabu@mavs.uta.edu>
2019-07-21 16:30:41 -0400

    Update OrderChain.R

--- OrderChain.R
+++ OrderChain.R
@@ -1,7 +1,14 @@
+library(httr)
+library(jsonlite)
+library(lubridate)
+library(rjson)
 #orders
 url <- "https://api.vertechain.com:6010/"
+newurl <- "https://api.projectverte.io:6010/api/vertechain/setOrder"
+authurl <- "https://api.projectverte.io:6010/users/authenticate"
+token <- content(POST(url = authurl, body = list(username = "Admin", password = "Admin123!"), encode = c("json")))$token
 missing <- matrix(c(0), nrow = 0, ncol = 1)
-for(i in 5001:5496){
+for(i in 1:25200){
 path <- gsub(" ","", paste("api/Vertechain/GetOrder?OrderID=VC003-",i))
 raw.result <- GET(url = url, path = path)
 if(length(content(raw.result)) == 0){
@@ -15,13 +22,17 @@ if(length(content(raw.result)) == 0){
 for(j in 1:n){
   transaction = content(raw.result)$transaction[j]
   JSON <- read_json(gsub(" ","",paste("https://api.vertechain.com:6010/api/VerteChain/GetOrderTransaction?key=",transaction)))
-  JSON<-JSON$order
+  JSON <- JSON$order
+  JSON$shipment$ReleaseOrders
+  JSON$shipment$ReleaseOrders[[1]]
+  JSON$shipment$ReleaseOrders[[1]]$ReleaseOrder <- NULL
+  JSON$shipment$ReleaseOrders[[1]]$Carton <- JSON$shipment$carton
+  JSON$shipment$carton <- NULL
   saveJSON <- toJSON(JSON)
-  write_json(saveJSON,path = gsub(" ", "", paste("~/Blockchain2.0/RScriptJSON/Orderfiles/", gsub(" ","", paste("VC003-",i,"(",j,")",".json")))))
+  POST(newurl, body = saveJSON, add_headers('Content-Type'='application/json','Authorization'='Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6IkFkbWluIiwicm9sZSI6IkFkbWluIiwibmJmIjoxNTYzNDg3NzE1LCJleHAiOjE1NjQwOTI1MTUsImlhdCI6MTU2MzQ4NzcxNX0.DqEiQtrxtubkNGgmWu4pBSGwdYxSiNGRJWbLpe4_oxw'))
 }
 }
-missing <- gsub(" ","",paste("VC003-",missing))
-write.csv(missing, file = "missingorders.csv")
 
+write.csv(missing, file = "missingorders.csv")
 
 
