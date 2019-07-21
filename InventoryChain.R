03bd566b6c3451d18ad2863a536c3a3633989b04
Arvindhan Rameshbabu <arvindhan.rameshbabu@mavs.uta.edu>
2019-07-21 16:31:18 -0400

    Update InventoryChain.R

--- InventoryChain.R
+++ InventoryChain.R
@@ -1,13 +1,17 @@
 #Inventory
+setwd("~/Blockchain2.0/RScriptJSON")
 url <- "https://api.vertechain.com:6010/"
-sku <- as.data.frame(read.csv("Inventory test file.csv", header = T, stringsAsFactors = F))
+newurl <- "https://api.projectverte.io:6010/api/Vertechain/SetInventory"
+authurl <- "https://api.projectverte.io:6010/users/authenticate"
+token <- content(POST(url = authurl, body = list(username = "Admin", password = "Admin123!"), encode = c("json")))$token
+INV <- as.data.frame(read.csv("Inventoryfiles.csv", header = T, stringsAsFactors = F))
 missinginv <- matrix(c(0), nrow = 0, ncol = 1)
 
-for(i in 1:nrow(sku)){
-  path <- gsub(" ","", paste("api/Vertechain/GetInventory?inventoryID=VIA-",sku[i,]))
+for(i in 1:nrow(INV)){
+  path <- gsub(" ","", paste("api/Vertechain/GetInventory?=",INV[i,]))
   raw.result <- GET(url = url, path = path)
   if(length(content(raw.result)) == 0){
-    b <- gsub(" ","",paste("VIA-",sku[i,]))
+    b <- INV[i,]
     missinginv <- rbind(missinginv,b)
     print(b)
     next
@@ -16,11 +20,11 @@ for(i in 1:nrow(sku)){
     n <- length(content(raw.result)$transaction)
   }
   for(j in 1:n){
-    transaction = content(raw.result)$transaction[j]
+    transaction = content(raw.result)$transaction[[j]][1]
     JSON <- read_json(gsub(" ","",paste("https://api.vertechain.com:6010/api/VerteChain/GetInventoryTransaction?key=",transaction)))
     JSON<-JSON$inventory
+    JSON <- JSON$facilityQty[[1]]$allocatedQty <- "0"
     saveJSON <- toJSON(JSON)
-    write_json(saveJSON,path = gsub(" ", "", paste("~/Blockchain2.0/RScriptJSON/Inventoryfiles/", gsub(" ","", paste(sku[i,],"(",j,")",".json")))))
+    POST(newurl, body = saveJSON, add_headers('Content-Type'='application/json','Authorization'='Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6IkFkbWluIiwicm9sZSI6IkFkbWluIiwibmJmIjoxNTYzNDg3NzE1LCJleHAiOjE1NjQwOTI1MTUsImlhdCI6MTU2MzQ4NzcxNX0.DqEiQtrxtubkNGgmWu4pBSGwdYxSiNGRJWbLpe4_oxw'))
   }
 }
-
